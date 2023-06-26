b8a116afcf83238174219e4ae2485aff8edbd69a
Sharath V <sharathkrml@gmail.com>
2023-06-26 13:40:56 +0530

    old-eth-registrar-controller-2

--- src/eth-registrar-controller.ts
+++ src/eth-registrar-controller.ts
@@ -1,68 +1,84 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  ETHRegistrarController,
-  NameRegistered,
-  NameRenewed,
-  NewPriceOracle,
-  OwnershipTransferred
+    NameRegistered as NameRegisteredEvent,
+    NameRenewed as NameRenewedEvent,
+    NewPriceOracle as NewPriceOracleEvent,
+    OwnershipTransferred as OwnershipTransferredEvent,
 } from "../generated/ETHRegistrarController/ETHRegistrarController"
-import { ExampleEntity } from "../generated/schema"
+import {
+    Counter,
+    OwnershipTransferred,
+    NewPriceOracle,
+    NameRenewed,
+    NameRegistered,
+} from "../generated/schema"
 
-export function handleNameRegistered(event: NameRegistered): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleNameRegistered(event: NameRegisteredEvent): void {
+    let counter = getCounter("NameRegistered")
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    let nameRegistered = new NameRegistered(counter.count.toString())
+    nameRegistered.index = counter.count
+    nameRegistered.hash = event.transaction.hash
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+    nameRegistered.name = event.params.name
+    nameRegistered.label = event.params.label
+    nameRegistered.owner = event.params.owner
+    nameRegistered.cost = event.params.cost
+    nameRegistered.expires = event.params.expires
+    nameRegistered.save()
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.name = event.params.name
-  entity.label = event.params.label
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.supportsInterface(...)
-  // - contract.makeCommitmentWithConfig(...)
-  // - contract.commitments(...)
-  // - contract.rentPrice(...)
-  // - contract.MIN_REGISTRATION_DURATION(...)
-  // - contract.minCommitmentAge(...)
-  // - contract.owner(...)
-  // - contract.isOwner(...)
-  // - contract.valid(...)
-  // - contract.available(...)
-  // - contract.maxCommitmentAge(...)
-  // - contract.makeCommitment(...)
+    counter.save()
 }
 
-export function handleNameRenewed(event: NameRenewed): void {}
+export function handleNameRenewed(event: NameRenewedEvent): void {
+    let counter = getCounter("NameRenewed")
 
-export function handleNewPriceOracle(event: NewPriceOracle): void {}
+    let nameRenewed = new NameRenewed(counter.count.toString())
+    nameRenewed.index = counter.count
+    nameRenewed.hash = event.transaction.hash
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+    nameRenewed.name = event.params.name
+    nameRenewed.label = event.params.label
+    nameRenewed.cost = event.params.cost
+    nameRenewed.expires = event.params.expires
+    nameRenewed.save()
+
+    counter.save()
+}
+
+export function handleNewPriceOracle(event: NewPriceOracleEvent): void {
+    let counter = getCounter("NewPriceOracle")
+
+    let newPriceOracle = new NewPriceOracle(counter.count.toString())
+    newPriceOracle.index = counter.count
+    newPriceOracle.hash = event.transaction.hash
+
+    newPriceOracle.oracle = event.params.oracle
+    newPriceOracle.save()
+
+    counter.save()
+}
+
+export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void {
+    let counter = getCounter("OwnershipTransferred")
+
+    let ownershipTransferred = new OwnershipTransferred(counter.count.toString())
+    ownershipTransferred.index = counter.count
+    ownershipTransferred.hash = event.transaction.hash
+
+    ownershipTransferred.previousOwner = event.params.previousOwner
+    ownershipTransferred.newOwner = event.params.newOwner
+    ownershipTransferred.save()
+
+    counter.save()
+}
+
+function getCounter(key: string): Counter {
+    let counter = Counter.load(key)
+    if (!counter) {
+        counter = new Counter(key)
+        counter.count = BigInt.fromI32(0)
+    }
+    counter.count = counter.count.plus(BigInt.fromI32(1))
+    return counter
+}
